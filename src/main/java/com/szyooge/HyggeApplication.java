4e122fc26c0980474f0fc4e9034d392995dd0803
quanyou.chen <quanyou.chen@SZXHITFKJJ007.bf.cn>
2017-05-11 11:05:13 +0800

    描述：init 提交人：chenquanyou

---
+++ src/main/java/com/szyooge/HyggeApplication.java
@@ -0,0 +1,22 @@
+package com.szyooge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+
+
+@SpringBootApplication
+@EnableCaching
+public class HyggeApplication {
+
+    @Bean
+    public MappingJackson2HttpMessageConverter json() {
+        return new MappingJackson2HttpMessageConverter();
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(HyggeApplication.class, args);
+    }
+}
